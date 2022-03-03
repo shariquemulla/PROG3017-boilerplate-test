b92f24e74e50c1e6c02cd82e223665c2f87400ac
Sharique Mulla <mullasharique@gmail.com>
2022-03-03 10:22:48 -0400

    New Issue added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {/* New Issue here @shariquemulla todo #1 */}
         <a
           className="App-link"
           href="https://reactjs.org"
